8efaec70300dcbd353dcc85547e63569dae857e3
Bodan Pittman <bodanpittman30@gmail.com>
2023-11-27 19:46:36 -0500

    Added Player documentation
    playa

--- app/src/main/java/com/example/demo_2340/Player.java
+++ app/src/main/java/com/example/demo_2340/Player.java
@@ -3,6 +3,9 @@ package com.example.demo_2340;
 import com.example.demo_2340.CollisionObserver.CollisionObserver;
 import com.example.demo_2340.Player_Movement.MovementStrategyPattern;
 
+/**
+ * Player class to represent the singleton player
+ */
 public class Player implements CollisionObserver {
     private static Player instance = null;
     private MovementStrategyPattern movementStrategy;
@@ -12,6 +15,9 @@ public class Player implements CollisionObserver {
 
     private int speed;
 
+    /**
+     * Base Constructor for singleton player
+     */
     private Player() {
         // Initialize default position and health
         this.xPosition = 0;
@@ -20,6 +26,10 @@ public class Player implements CollisionObserver {
         this.speed = 1;
     }
 
+    /**
+     * getInstance Method
+     * @return static player and creates if doesn't exist
+     */
     public static Player getInstance() {
         if (instance == null) {
             instance = new Player();
@@ -27,25 +37,50 @@ public class Player implements CollisionObserver {
         return instance;
     }
 
+    /**
+     * positionGetter
+     * @return int position
+     */
     public int getxPosition() {
         return xPosition;
     }
 
+    /**
+     * positionSetter for X
+     * @param xPosition int for movement
+     */
     public void setxPosition(int xPosition) {
         this.xPosition = xPosition;
     }
 
+    /**
+     * Getter for y Position
+     * @return int representing y position
+     */
     public int getyPosition() {
         return yPosition;
     }
 
+    /**
+     * setter for y position
+     * @param yPosition int
+     */
     public void setyPosition(int yPosition) {
         this.yPosition = yPosition;
     }
 
+    /**
+     * movementStrategy Setter
+     * @param movementStrategy to represent pattern
+     */
     public void setMovementStrategy(MovementStrategyPattern movementStrategy) {
         this.movementStrategy = movementStrategy;
     }
+
+    /**
+     * onCollisionDetection method
+     * @param damage amount to hurt player
+     */
     @Override
     public void onCollisionDetected(int damage) {
         // Handle collision
@@ -61,21 +96,41 @@ public class Player implements CollisionObserver {
         }
     }
 
+    /**
+     * health getter
+     * @return int for health
+     */
     public int getHealth() {
         return health;
     }
 
+    /**
+     * setter for health
+     * @param int representing health
+     */
     public void setHealth(int h) {
         health = h;
     }
 
+    /**
+     * getter for Speed
+     * @return int speed
+     */
     public int getSpeed() {
         return speed;
     }
 
+    /**
+     * setter for speed
+     * @param speed int for speed
+     */
     public void setSpeed(int speed) {
         this.speed = speed;
     }
+
+    /**
+     * endGame Method
+     */
     private void endGame() {
 
     }
